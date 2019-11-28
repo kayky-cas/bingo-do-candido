3abffcbbcb1fe9862002ef2ea290ecddc5894e3f
kayky-cas <57455569+kayky-cas@users.noreply.github.com>
2019-11-28 13:15:13 -0300

    3

---
+++ geraNum.php
@@ -0,0 +1,6 @@
+<?php
+  for ($i=0; $i < 25; $i++) {
+    $tabela[$i] = rand(0,100);
+  }
+  echo json_encode($tabela);
+?>
---
+++ script/script.js
@@ -0,0 +1,21 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    cartela = JSON.parse(this.responseText)
+    for (var i = 2; i < 27; i++) {
+      document.getElementsByTagName('div')[i].innerHTML = cartela[i-2]
+    }
+  }
+}
+xhttp.open("GET", "geraCartela.php", true);
+xhttp.send();
+
+function numAlet() {
+  var xhttpNUM = new XMLHttpRequest();
+  xhttpNUM.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    }
+  }
+  xhttpNUM.open("GET", "geraNUM.php", true);
+  xhttpNUM.send();
+}
