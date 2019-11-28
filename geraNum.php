ede9a18424e1e4d440cccf86ce89c35b893201a0
kayky-cas <57455569+kayky-cas@users.noreply.github.com>
2019-11-28 14:00:45 -0300

    6

--- geraNum.php
+++ geraNum.php
@@ -1,17 +1,4 @@
 <?php
-  $numJogadosArq = fopen("numJogados.txt","a");
-  do {
-    $numA = rand(0,100);
-    $cont = 0;
-    for ($i=0; $i < filesize("numJogados.txt"); $i++) {
-      if ($numA == fread($numJogadosArq,i)) {
-        $cont = 1;
-      }
-    }
-
-  } while ($cont!=0);
-  fwrite($numJogadosArq, $numA+"\n");
-  fclose($numJogadosArq);
-  // $tabela[$i] = rand(0,100);
+  $numA = rand(0,100);
   echo json_encode($numA);
 ?>
